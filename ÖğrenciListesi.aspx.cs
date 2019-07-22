6b57b000c2c2da5b0d3aa0598111373ce59a18b2
Sarper-Bal <49680723+Sarper-Bal@users.noreply.github.com>
2019-07-22 13:55:32 +0300

    Create ÖğrenciListesi.aspx.cs

---
+++ ÖğrenciListesi.aspx.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using EntityLayer;
+using DataAccessLayer;
+using BusinessLogicLayer;
+
+public partial class ÖğrenciListesi : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        List<EntityOgrenci> OgrList = BLLOgrenci.BllListele();
+        Repeater1.DataSource = OgrList;
+        Repeater1.DataBind();
+
+
+    }
+}
