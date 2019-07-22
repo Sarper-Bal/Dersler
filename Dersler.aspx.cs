1c3c78af6ea2d051d69303040abf3073bb2a31c9
Sarper-Bal <49680723+Sarper-Bal@users.noreply.github.com>
2019-07-22 13:45:17 +0300

    Create Dersler.aspx.cs

---
+++ Dersler.aspx.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using EntityLayer;
+using DataAccessLayer;
+using BusinessLogicLayer;
+
+public partial class Dersler : System.Web.UI.Page
+{ 
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Page.IsPostBack == false)
+        {
+            List<EntityDers> EntDers = BLLDers.BllListele();
+            DropDownList1.DataSource = BLLDers.BllListele();
+            DropDownList1.DataTextField = "DERSAD";
+            DropDownList1.DataValueField = "ID";
+            DropDownList1.DataBind();
+        }
+
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        EntityBasvuruForm ent = new EntityBasvuruForm();
+        ent.BASOGRID = int.Parse(TextBox1.Text);
+        ent.BASDERSID = int.Parse(DropDownList1.SelectedValue.ToString());
+        BLLDers.TalepEkleBll(ent);
+    }
+}
